eedeb6488577417c8d27e3e701eba6a806b63884
zm17645793671 <zm17645793671@163.com>
2019-03-08 13:17:17 +0800

    第三次提交

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,6 +1,12 @@
 class TestLogin:
     def test01(self):
-        assert 0
+        assert 1
+
     def test02(self):
         assert 1
 
+    def test03(self):
+        assert 1
+
+    def test04(self):
+        assert 1
