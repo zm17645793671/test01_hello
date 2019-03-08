27f9e60bc2440b026222d97606d71e3dc7808132
zm17645793671 <zm17645793671@163.com>
2019-03-08 16:11:12 +0800

    第6次提交

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -12,10 +12,10 @@ class TestLogin:
         assert 1
 
     def test05(self):
-        assert 0
+        assert 1
 
     def test06(self):
-        assert 0
+        assert 1
 
     def test07(self):
         assert 0
